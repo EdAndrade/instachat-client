78296bea4bc01633881e65487538b5f47adf5c15
edAndrade <edmilisonwork@gmail.com>
2022-01-02 22:17:09 +0100

    fixed dependencies versions

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,23 @@
 <template>
-  <Tutorial />
+
+	<section id="indexSection">
+
+
+	</section>
+
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
+	import Vue from "vue";
 
-export default Vue.extend({})
+	export default Vue.extend({});
 </script>
+
+<style scoped lang="scss">
+
+	#indexSection{
+		width: 100%;
+		height: 100vh;
+		background-color: #332441;
+	}
+</style>
