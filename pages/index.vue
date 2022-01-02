c3ffd334afd1f05d4a53b0f02182f29f405abaa7
edAndrade <edmilisonwork@gmail.com>
2022-01-02 22:29:38 +0100

    add some style to index

--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,23 @@
 
 	<section id="indexSection">
 
+		<div id="top">
+
+			<div id="introduction">
+
+				<div class="title">
+					<p>Eazy and instant chat rooms</p>
+				</div>
+
+				<div class="desc">
+					fdjsflsjdflkasdjf;ldksfj;sldkfjs;dlkfjs;flkajds;flksjdflksdfj;lksdfj;lksdfj;sdlkfj;sdlkfj;asdlfkjsa;ldfkjsd;lfkja;sdflkj;l
+				</div>
+			</div>
+
+			<div id="enterAToChatRoom">
+				<button>Get into a chat room</button>
+			</div>
+		</div>
 
 	</section>
 
@@ -19,5 +36,32 @@
 		width: 100%;
 		height: 100vh;
 		background-color: #332441;
+		padding: 40px;
+
+		#top{
+			display: flex;
+			justify-content: space-between;
+
+			#introduction{
+				
+				.title{
+					width: 540px;
+					font-size: 4em;
+					font-family: 'Gravitas One';
+					color: #fff;
+				}
+			}
+
+			#enterAToChatRoom{
+
+				button{
+					width: 158px;
+					background-color: #5B238C;
+					border-radius: 5px;
+					padding: 10px;
+					color: #fff;
+				}
+			}
+		}
 	}
 </style>
