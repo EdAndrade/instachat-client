eea13c162a0f517684229d80de2a8ca809e48f31
edAndrade <edmilisonwork@gmail.com>
2022-03-30 15:11:48 +0100

    created chatOPtions modal

---
+++ components/ChatOptions/index.vue
@@ -0,0 +1,26 @@
+<template>
+
+    <section id="ChatOptions" @click.self="$emit('closeChatOptions')">
+
+        <div class="closeButton" @click="$emit('closeChatOptions')">
+            <i class="fas fa-close"></i>
+        </div>
+
+        <div id="box">
+
+            <div class="title">
+                <p>Opções</p>
+            </div>
+            
+            <div class="optionsContent">
+
+            </div>
+            
+        </div>
+    </section>
+
+</template>
+
+<script lang="ts" src="./script.ts"></script>
+
+<style lang="scss" src="./style.scss" scoped></style>
